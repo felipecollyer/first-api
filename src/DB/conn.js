0bf3ab76edfbf8a50e520d991b8d87c902167e84
felipe Collyer <felipecollyer@hotmail.com>
2023-09-30 12:24:59 -0300

    Remove: arquivo de conexao

--- src/DB/conn.js
+++ src/DB/conn.js
@@ -2,17 +2,10 @@ const { Client } = require("pg");
 
 const conn = new Client({
   user: "postgres",
-  password: "123456",
-  database: "postgres",
+  password: "felipe",
+  database: "first_api",
   host: "localhost", // ou o endereço do servidor do PostgreSQL
   port: 5432,
-})
-  .connect()
-  .then(() => {
-    console.log("Conexão bem-sucedida com o banco de dados PostgreSQL");
-  })
-  .catch((error) => {
-    console.error("Erro ao conectar ao banco de dados:", error);
-  });
+});
 
 module.exports = conn;
