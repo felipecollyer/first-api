d1cdaf825cbda3c04b3f322fe86923299c4220ec
felipe Collyer <felipecollyer@hotmail.com>
2023-10-01 12:15:53 -0300

    ADD:  crud usuarios

--- src/Router/index.js
+++ src/Router/index.js
@@ -8,34 +8,72 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get("/user", function (req, res) {
-  res.send("Read user");
+  const sql = "SELECT * FROM usuarios";
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log("errow");
+    } else {
+      res.send(data.rows);
+    }
+  });
 });
 
-router.get("/test", function (req, res) {
-  res.send("xxxx user");
+router.get("/user/:id", function (req, res) {
+  const idParams = req.params.id;
+
+  const sql = `SELECT * FROM usuarios WHERE id =${idParams}`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log("errow");
+    } else {
+      res.send(data.rows);
+    }
+  });
 });
 
 router.post("/user", async function (req, res) {
   const payload = req.body;
 
-  const sql = `INSERT INTO usuarios (email, senha) VALUES ('${payload.email}','${payload.password}')`;
+  const sql = `INSERT INTO usuarios (email, senha) VALUES ('${payload.email}','${payload.senha}')`;
 
   conn.query(sql, function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log("deu bom");
-      res.end();
+      res.send(`Usuario ${payload.email} criado com sucesso`);
     }
   });
 });
 
-router.put("/user", function (req, res) {
-  res.send("Update user");
+router.put("/user/:id", function (req, res) {
+  const { id } = req.params;
+  const { email, senha } = req.body;
+
+  const sql = `UPDATE usuarios SET email = '${email}', senha = '${senha}' WHERE id = ${id} RETURNING *`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("Dados atualizados");
+    }
+  });
 });
 
-router.delete("/user", function (req, res) {
-  res.send("Delete user");
+router.delete("/user/:id", function (req, res) {
+  const { id } = req.params;
+
+  const sql = `DELETE FROM usuarios WHERE id = ${id}`;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      res.send(`Usuario com ID ${id}, deletado!`);
+    }
+  });
 });
 
 module.exports = router;
