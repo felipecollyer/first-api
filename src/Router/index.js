fa299c7d5d9e8a2621f4c2b5fdcef880a03003af
felipe Collyer <felipecollyer@hotmail.com>
2023-10-03 22:38:54 -0300

    ALT: middleware import

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,16 +1,11 @@
 const Express = require("express");
-
+const Middlewares = require("../Middlewares");
 const Router = Express.Router();
 const Controler = require("../Controller/User");
 
-Router.use(function timeLog(req, res, next) {
-  console.log("Time: ", Date.now());
-  next();
-});
+Router.get("/user", Middlewares.log1, Controler.ReadAllUser);
 
-Router.get("/user", Controler.ReadAllUser);
-
-Router.get("/user/:id", Controler.ReadOneUser);
+Router.get("/user/:id", Middlewares.log2, Controler.ReadOneUser);
 
 Router.post("/user", Controler.Createuser);
 
